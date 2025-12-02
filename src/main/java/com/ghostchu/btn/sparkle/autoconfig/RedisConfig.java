b0790ee3e8d5445adca45edc2fdc3f4773ee40bc
Ghost_chu <ghostchu@111.com>
2025-12-03 00:36:10 +0800

    切换序列化器

--- src/main/java/com/ghostchu/btn/sparkle/autoconfig/RedisConfig.java
+++ src/main/java/com/ghostchu/btn/sparkle/autoconfig/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -18,17 +19,17 @@ public class RedisConfig {
         template.setKeySerializer(redisSerializer);
         template.setValueSerializer(redisSerializer);
         template.setConnectionFactory(factory);
-
         return template;
     }
 
     @Bean("stringLongRedisTemplate")
     public RedisTemplate<String, Long> stringLongRedisTemplate(@NotNull RedisConnectionFactory factory) {
         RedisSerializer<@NotNull String> redisSerializer = new StringRedisSerializer();
+        JacksonJsonRedisSerializer<@NotNull Long> jacksonJsonRedisSerializer = new JacksonJsonRedisSerializer<>(Long.class);
         RedisTemplate<String, Long> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
         template.setKeySerializer(redisSerializer);
-        template.setValueSerializer(redisSerializer);
+        template.setValueSerializer(jacksonJsonRedisSerializer);
         return template;
     }
 }
