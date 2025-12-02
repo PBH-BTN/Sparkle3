18857e98f0d2a994d86a8ee320dbe6887ec63d67
Ghost_chu <ghostchu@111.com>
2025-12-03 01:34:12 +0800

    没开 Scheduling 太尴尬了（

--- src/main/java/com/ghostchu/btn/sparkle/SparkleApplication.java
+++ src/main/java/com/ghostchu/btn/sparkle/SparkleApplication.java
@@ -3,11 +3,17 @@ package com.ghostchu.btn.sparkle;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.retry.annotation.EnableRetry;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @MapperScan("com.ghostchu.btn.sparkle.mapper")
 @EnableAsync
+@EnableScheduling
+@EnableRetry
+@EnableCaching
 public class SparkleApplication {
 
     public static void main(String[] args) {
