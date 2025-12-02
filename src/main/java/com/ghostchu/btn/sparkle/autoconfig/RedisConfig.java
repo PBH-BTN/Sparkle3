ced8fba366b8c57b742b48e60315d1fc9b682c55
Ghost_chu <ghostchu@111.com>
2025-12-03 00:28:40 +0800

    设置 StringSerializer

--- src/main/java/com/ghostchu/btn/sparkle/autoconfig/RedisConfig.java
+++ src/main/java/com/ghostchu/btn/sparkle/autoconfig/RedisConfig.java
@@ -5,21 +5,30 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
     @Bean("stringStringRedisTemplate")
     public RedisTemplate<String, String> stringStringRedisTemplate(@NotNull RedisConnectionFactory factory) {
+        RedisSerializer<@NotNull String> redisSerializer = new StringRedisSerializer();
         RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setKeySerializer(redisSerializer);
+        template.setValueSerializer(redisSerializer);
         template.setConnectionFactory(factory);
+
         return template;
     }
 
     @Bean("stringLongRedisTemplate")
     public RedisTemplate<String, Long> stringLongRedisTemplate(@NotNull RedisConnectionFactory factory) {
+        RedisSerializer<@NotNull String> redisSerializer = new StringRedisSerializer();
         RedisTemplate<String, Long> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
+        template.setKeySerializer(redisSerializer);
+        template.setValueSerializer(redisSerializer);
         return template;
     }
 }
