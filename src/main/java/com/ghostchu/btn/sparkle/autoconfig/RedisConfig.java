8007286ad9b30929bff4f879033bba31610a60ed
Ghost_chu <ghostchu@111.com>
2025-12-03 00:45:59 +0800

    fuck redis 2

--- src/main/java/com/ghostchu/btn/sparkle/autoconfig/RedisConfig.java
+++ src/main/java/com/ghostchu/btn/sparkle/autoconfig/RedisConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -16,15 +17,23 @@ public class RedisConfig {
         RedisSerializer<@NotNull String> redisSerializer = new StringRedisSerializer();
         RedisTemplate<String, String> template = new RedisTemplate<>();
         template.setKeySerializer(redisSerializer);
+        template.setHashKeySerializer(redisSerializer);
         template.setValueSerializer(redisSerializer);
+        template.setHashValueSerializer(redisSerializer);
         template.setConnectionFactory(factory);
         return template;
     }
 
     @Bean("stringLongRedisTemplate")
     public RedisTemplate<String, Long> stringLongRedisTemplate(@NotNull RedisConnectionFactory factory) {
+        RedisSerializer<@NotNull String> stringSerializer = new StringRedisSerializer();
+        RedisSerializer<@NotNull Long> jsonSerializer = new JacksonJsonRedisSerializer<>(Long.class);
         RedisTemplate<String, Long> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
+        template.setKeySerializer(stringSerializer);
+        template.setHashKeySerializer(stringSerializer);
+        template.setValueSerializer(jsonSerializer);
+        template.setHashValueSerializer(jsonSerializer);
         return template;
     }
 }
