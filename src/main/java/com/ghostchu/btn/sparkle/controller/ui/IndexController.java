2227ec4eaf061985932e7c0b7a37131338e874cb
Ghost_chu <ghostchu@111.com>
2025-12-07 22:09:58 +0800

    添加首页更新数据

--- src/main/java/com/ghostchu/btn/sparkle/controller/ui/IndexController.java
+++ src/main/java/com/ghostchu/btn/sparkle/controller/ui/IndexController.java
@@ -26,21 +26,21 @@ public class IndexController extends AbstractSparkleMVC {
     @GetMapping("/")
     public String index(@AuthenticationPrincipal SparkleUserDetails userDetails, Model model) {
         // 封禁数量
-        model.addAttribute("stats_banhistory_alltime", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_BANHISTORY_ALLTIME));
-        model.addAttribute("stats_banhistory_30days", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_BANHISTORY_30DAYS));
-        model.addAttribute("stats_banhistory_14days", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_BANHISTORY_14DAYS));
-        model.addAttribute("stats_banhistory_7days", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_BANHISTORY_7DAYS));
-        model.addAttribute("stats_banhistory_24hours", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_BANHISTORY_24HOURS));
+        model.addAttribute("stats_banhistory_alltime", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_BANHISTORY_ALLTIME.getKey()));
+        model.addAttribute("stats_banhistory_30days", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_BANHISTORY_30DAYS.getKey()));
+        model.addAttribute("stats_banhistory_14days", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_BANHISTORY_14DAYS.getKey()));
+        model.addAttribute("stats_banhistory_7days", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_BANHISTORY_7DAYS.getKey()));
+        model.addAttribute("stats_banhistory_24hours", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_BANHISTORY_24HOURS.getKey()));
         // 跟踪 Peers
-        model.addAttribute("stats_swarmtracker_alltime", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_SWARMTRACKER_ALLTIME));
-        model.addAttribute("stats_swarmtracker_14days", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_SWARMTRACKER_14DAYS));
-        model.addAttribute("stats_swarmtracker_7days", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_SWARMTRACKER_7DAYS));
-        model.addAttribute("stats_swarmtracker_24hours", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_SWARMTRACKER_24HOURS));
+        model.addAttribute("stats_swarmtracker_alltime", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_SWARMTRACKER_ALLTIME.getKey()));
+        model.addAttribute("stats_swarmtracker_14days", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_SWARMTRACKER_14DAYS.getKey()));
+        model.addAttribute("stats_swarmtracker_7days", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_SWARMTRACKER_7DAYS.getKey()));
+        model.addAttribute("stats_swarmtracker_24hours", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_SWARMTRACKER_24HOURS.getKey()));
         // 活动 UserApps
-        model.addAttribute("stats_userapp_30days", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_USERAPP_30DAYS));
-        model.addAttribute("stats_userapp_14days", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_USERAPP_14DAYS));
-        model.addAttribute("stats_userapp_7days", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_USERAPP_7DAYS));
-        model.addAttribute("stats_userapp_24hours", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_USERAPP_24HOURS));
+        model.addAttribute("stats_userapp_30days", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_USERAPP_30DAYS.getKey()));
+        model.addAttribute("stats_userapp_14days", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_USERAPP_14DAYS.getKey()));
+        model.addAttribute("stats_userapp_7days", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_USERAPP_7DAYS.getKey()));
+        model.addAttribute("stats_userapp_24hours", stringLongRedisTemplate.opsForValue().get(RedisKeyConstant.STATS_USERAPP_24HOURS.getKey()));
         return "index";
     }
 }
