2c873bff8781b0acb4583fa728cb80a2d74d436a
Ghost_chu <ghostchu@111.com>
2025-12-10 14:25:22 +0800

    This is not a tracker

--- src/main/java/com/ghostchu/btn/sparkle/controller/whatthefuck/AnnounceController.java
+++ src/main/java/com/ghostchu/btn/sparkle/controller/whatthefuck/AnnounceController.java
@@ -1,6 +1,7 @@
 package com.ghostchu.btn.sparkle.controller.whatthefuck;
 
 import org.jetbrains.annotations.NotNull;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AnnounceController {
     @GetMapping(value = {"/announce", "/tracker/announce"})
     public ResponseEntity<@NotNull String> handleAnnounce() {
-
+        // return a bencoded string that idencate this not a tracker, with retry in never
+        return ResponseEntity.badRequest().contentType(MediaType.TEXT_PLAIN).body("d14:failure reason23:This is not a tracker.e15:retry in nevere");
     }
 }
