bf9fc5a21385b5027699ee83907341b1e835bde0
Ghost_chu <ghostchu@111.com>
2026-01-12 22:34:52 +0800

    提交测试

---
+++ src/main/java/com/ghostchu/btn/sparkle/controller/debug/SwarmStatisticDebugController.java
@@ -0,0 +1,31 @@
+package com.ghostchu.btn.sparkle.controller.debug;
+
+import com.ghostchu.btn.sparkle.service.impl.UserSwarmStatisticsServiceImpl;
+import jakarta.websocket.server.PathParam;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.OffsetDateTime;
+import java.util.StringJoiner;
+
+
+@Controller
+public class SwarmStatisticDebugController {
+
+    @Autowired
+    private UserSwarmStatisticsServiceImpl userSwarmStatisticsService;
+
+    @GetMapping("/debug/swarmStatistic/perUser/{uid}")
+    public ResponseEntity<@NotNull String> debugPerUserSwarmStatistic(@PathParam("uid") Long uid) {
+        StringJoiner joiner = new StringJoiner("\n");
+        joiner.add("DEBUG: Swarm Statistic for User " + uid);
+        OffsetDateTime sevenDaysAgo = OffsetDateTime.now().minusDays(7);
+        OffsetDateTime now = OffsetDateTime.now();
+        var result = userSwarmStatisticsService.generateUserSwarmStatistics(uid, sevenDaysAgo, now);
+        joiner.add("Result: " + result);
+        return ResponseEntity.ok(joiner.toString());
+    }
+}
