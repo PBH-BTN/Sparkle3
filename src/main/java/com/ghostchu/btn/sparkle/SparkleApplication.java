f8e09556db325fc7bb816d47a2ee994d9c6273ea
Ghost_chu <ghostchu@111.com>
2026-02-04 15:39:33 +0800

    还是要加回来

--- src/main/java/com/ghostchu/btn/sparkle/SparkleApplication.java
+++ src/main/java/com/ghostchu/btn/sparkle/SparkleApplication.java
@@ -10,7 +10,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @MapperScan("com.ghostchu.btn.sparkle.mapper")
 //@EnableAsync
 @EnableScheduling
