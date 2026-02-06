0e7a6b477ed890357c723b5f4d48d73e8c36bf06
Ghost_chu <ghostchu@111.com>
2026-02-06 19:58:51 +0800

    Revert "继续 vibe"
    This reverts commit 8eeb0da10477b546006fc2ddd02df1e71ea1366d.

--- src/main/java/com/ghostchu/btn/sparkle/mapper/clickhouse/SwarmStatisticsClickHouseMapper.java
+++ src/main/java/com/ghostchu/btn/sparkle/mapper/clickhouse/SwarmStatisticsClickHouseMapper.java
@@ -1,6 +1,5 @@
 package com.ghostchu.btn.sparkle.mapper.clickhouse;
 
-import com.baomidou.dynamic.datasource.annotation.DS;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.ghostchu.btn.sparkle.entity.UserSwarmStatistic;
 import com.ghostchu.btn.sparkle.service.dto.UserSwarmStatisticAggregationDto;
@@ -15,7 +14,6 @@ import java.util.List;
  * This mapper is used to read aggregated data from ClickHouse
  * All methods use ClickHouse-compatible SQL dialects
  */
-@DS("clickhouse")
 public interface SwarmStatisticsClickHouseMapper extends BaseMapper<UserSwarmStatistic> {
 
     /**
